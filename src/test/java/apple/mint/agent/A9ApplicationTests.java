4df664ca2b018e47606d6eb87eaca14005db0b53
whoana <whoana@gmail.com>
2022-11-23 08:48:48 +0900

    first commit

---
+++ src/test/java/apple/mint/agent/A9ApplicationTests.java
@@ -0,0 +1,13 @@
+package apple.mint.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
